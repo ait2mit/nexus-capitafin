33e0e7a3a9b14f3062fa252ff04cf6a18cf2c046
M. Shafiqul Islam <ait2mit@yahoo.com>
2022-08-24 20:46:10 -0400

    Google account security updated

--- core/settings.py
+++ core/settings.py
@@ -199,5 +199,5 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'dwaterbd@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ce9504028'
+EMAIL_HOST_PASSWORD = 'kuhfxvpjzolcytwz'
 EMAIL_PORT = 587
